8861b09af219dfced849e7f6e23311676d9229e1
Welinton Borges <Welinton.b.borges@gmail.com>
2023-11-09 14:47:11 -0300

    Seller entity class e teste do mesmo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 import com.example.demo.entities.Department;
+import com.example.demo.entities.Seller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -14,6 +17,11 @@ public class DemoApplication {
 
     Department obj = new Department(1, "Books");
     System.out.println(obj);
+
+    System.out.println("---Testando Objeto Seller---");
+
+    Seller seller = new Seller(21, "Bob","bob@gmail.com", new Date(), 4000.0, obj);
+    System.out.println(seller);
   }
 
 }
