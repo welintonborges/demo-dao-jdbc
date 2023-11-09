a2cf4f700b09d1d9312f6867bea2c865a12621ba
Welinton Borges <Welinton.b.borges@gmail.com>
2023-11-09 13:23:46 -0300

    Departament entity class e teste do mesmo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.entities.Department;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,11 @@ public class DemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
+
+    System.out.println("---Testando Objeto departamento---");
+
+    Department obj = new Department(1, "Books");
+    System.out.println(obj);
   }
 
 }
