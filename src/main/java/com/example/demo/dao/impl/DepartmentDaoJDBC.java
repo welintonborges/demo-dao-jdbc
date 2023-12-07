927cd2f8ab3e545daf9073d5be6ff767cf01156a
Welinton Borges <Welinton.b.borges@gmail.com>
2023-12-07 12:02:14 -0300

    Update implementation  class DepartmentDaoJDBC

--- src/main/java/com/example/demo/dao/impl/DepartmentDaoJDBC.java
+++ src/main/java/com/example/demo/dao/impl/DepartmentDaoJDBC.java
@@ -1,6 +1,7 @@
 package com.example.demo.dao.impl;
 
 import com.example.demo.dao.DepartmentDao;
+import com.example.demo.db.DB;
 import com.example.demo.db.DbException;
 import com.example.demo.entities.Department;
 
@@ -49,7 +50,24 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
   @Override
   public void update(Department obj) {
+    PreparedStatement st = null;
 
+    try {
+      st = conn.prepareStatement(
+        "UPDATE department " +
+          "SET Name = ? " +
+          "WHERE Id = ?");
+
+      st.setString(1, obj.getName());
+      st.setInt(2, obj.getId());
+
+      st.executeUpdate();
+
+    } catch (SQLException e) {
+        throw new DbException(e.getMessage());
+    }finally {
+      DB.closeStatement(st);
+    }
   }
 
   @Override
